47e28eaa8419d733291bb47893261944bde70aed
AnneKsatn <36439886+AnneKsatn@users.noreply.github.com>
2022-07-19 00:54:00 +0300

    working

--- worker-app/src/app/system/assignment-info/step-page/step-page.component.ts
+++ worker-app/src/app/system/assignment-info/step-page/step-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StepService } from 'src/app/shared/services/step.service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Observable, ReplaySubject } from 'rxjs';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-step-page',
@@ -14,13 +17,19 @@ export class StepPageComponent implements OnInit {
   stepId!: string;
   step!: any;
   image_src: any;
+  report_file: any;
+
+  time_start: Date;
 
   constructor(
+    private sanitizer: DomSanitizer,
     private activatedRouter: ActivatedRoute,
     private stepService: StepService,
     private storage: AngularFireStorage,
     private router: Router
-  ) { }
+  ) {
+    this.time_start = new Date()
+  }
 
   ngOnInit(): void {
     this.activatedRouter.queryParams.subscribe((params: any) => {
@@ -29,7 +38,6 @@ export class StepPageComponent implements OnInit {
 
       this.stepService.getStep(this.assignmentId, this.stepId).subscribe((step: any) => {
         this.step = step.data()
-
 
         const ref = this.storage.ref(this.step.file);
         ref.getDownloadURL().subscribe((data: any) => {
@@ -40,8 +48,61 @@ export class StepPageComponent implements OnInit {
     })
   }
 
+  fileName: any
+
+  onFileSelected(event: any) {
+
+    const file: File = event.target.files[0];
+    this.report_file = file;
+
+    console.log("UPLOADED")
+
+    if (file) {
+      // this.fileName = file.name;
+      const formData = new FormData();
+      formData.append("thumbnail", file);
+    }
+
+    this.convertFile(event.target.files[0]).subscribe(base64 => {
+      console.log(base64)
+
+      this.fileName = this.sanitizer.bypassSecurityTrustResourceUrl(base64)
+      // this.step.report_string = base64
+      // this.step.report_string = 'data:image/png;base64,' + base64
+    });
+  }
+
+
+  convertFile(file: File): Observable<string> {
+    const result = new ReplaySubject<string>(1);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    // reader.readAsBinaryString(file);
+    // reader.onload = (event) => result.next(btoa(event?.target!.result!.toString()));
+    reader.onload = () => result.next(reader.result!.toString())
+    return result;
+  }
+
   completeStep() {
+
+    let elapsed_seconds = Math.abs((new Date().getTime() - this.time_start.getTime()) / 1000);
+
+    let report_file_name = "";
+
+    if (this.report_file) {
+      report_file_name = '/images' + Math.random() + this.report_file
+
+      const ref = this.storage.ref(report_file_name);
+      const task = ref.put(this.report_file);
+    }
+
+    this.step.report_file = report_file_name
     this.step.status = "completed";
+    this.step.elapsed_seconds = elapsed_seconds
+
+
+    console.log(this.step)
+
     this.stepService.updateStep(this.assignmentId, this.stepId, this.step).then((data: any) => {
       this.router.navigate(["system/assignment"], { queryParams: { assignment_id: this.assignmentId } })
     })
