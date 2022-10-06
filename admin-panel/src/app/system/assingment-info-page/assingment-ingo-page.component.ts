020b6eb03aecc58d2989a69483301eda945dceab
AnneKsatn <36439886+AnneKsatn@users.noreply.github.com>
2022-10-07 00:35:36 +0300

    photo saved

--- admin-panel/src/app/system/assingment-info-page/assingment-ingo-page.component.ts
+++ admin-panel/src/app/system/assingment-info-page/assingment-ingo-page.component.ts
@@ -50,9 +50,13 @@ export class AssingmentIngoPageComponent implements OnInit {
         this.assignment.steps.forEach((step: any) => {
           this.stepService.getAssignStepById(step.id, params['id']).subscribe((data: any) => {
             let step = data.data()
+            step.id = data.id
+
             console.log(step)
 
             console.log(step.report_file)
+            console.log(step.file)
+
 
             if (step.report_file) {
               const ref = this.storage.ref(step.report_file);
@@ -104,17 +108,100 @@ export class AssingmentIngoPageComponent implements OnInit {
     doc.text(this.assignment.task_title, x, y)
     y = y + 40;
 
-    this.steps.forEach((step: any) => {
-      if (step.report_string) {
+    this.steps.forEach(async (step: any) => {
 
-        doc.text(step.title, x, y)
-        y = y + 40;
+      console.log(step.checklist)
+      doc.text(step.title, x, y)
+      y = y + 40;
 
-        doc.addImage(step.report_string, 'PNG', x, y, width, height)
-        y = y + height + 20;
+      doc.text("Checklist", x, y)
+      y = y + 30;
+
+      step.checklist.forEach((item: any) => {
+        doc.text(item.title, x, y)
+        x = x + 300
+        doc.text(item.isChecked.toString(), x, y)
+        x = x - 300
+        y = y + 20
+      })
+
+      if (step.report_file) {
+
+
+        const elemen = document.getElementById(step.id) as HTMLImageElement
+
+        let imageLoaded: HTMLImageElement = await this.getDataUri(elemen.src);
+        let pdf = new jsPDF('p', 'pt', 'a4');
+
+
+        const img = new Image();
+        img.src = elemen.src;
+        img.onload = () => {
+          // await for the image to be fully loaded
+
+          console.log(img)
+          doc.addImage(img, 'png', x, y, 250, 250);
+
+          doc.save("customFonts.pdf");
+          // doc.text("Sample", 20, 15);
+          // //...
+          // doc.save('order.pdf');
+        };
+
+        // pdf.addImage(elemen.src, "PNG", 0, 0, 250, 250);
+
+        // var img = document.createElement('img') as HTMLImageElement;
+        // img.src = step.report_file;
+
+        // console.log(img)
+        // doc.addImage(img, 'JPEG', 20, 20, 400, 150);
+
+        // const elemen = document.getElementById(step.id) as HTMLImageElement
+        // // // console.log(elemen, "ELEMEN")
+        // console.log(elemen.src)
+
+
+        // doc.addImage(elemen, 'PNG', 20, 20, 400, 150);
+        // y = y + 40;
+        // var myImage = new Image();
+        // myImage.src = step.report_file;
+
+        // myImage.onload = function () {
+        //   doc.addImage(myImage, 'png', x, y, width, height)
+        //   y = y + height + 20;
+        // };
+
+        // const srcpath = canvas.toDataURL(step.report_file);
+        // imgageData.src = srcpath;
+        // doc.addImage(imgageData, 'PNG', 20, 20, 400, 150);
       }
+
+
+      // doc.addImage(step.report_string, 'PNG', x, y, width, height)
+      // y = y + height + 20;
+
+      // if (step.report_string) {
+
+      //   doc.text(step.title, x, y)
+      //   y = y + 40;
+
+      //   doc.addImage(step.report_string, 'PNG', x, y, width, height)
+      //   y = y + height + 20;
+      // }
     })
 
     doc.save("customFonts.pdf");
+  }
+
+  async getDataUri(url: string): Promise<HTMLImageElement> {
+    return new Promise(resolve => {
+      var image = new Image();
+
+      image.onload = () => {
+        resolve(image);
+      };
+
+      image.src = url;
+    })
   }
 }
