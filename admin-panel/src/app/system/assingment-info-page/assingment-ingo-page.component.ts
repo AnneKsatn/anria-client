d1df07f36189c8f407b5df891b1530099e03c35b
AnneKsatn <36439886+AnneKsatn@users.noreply.github.com>
2022-10-07 09:16:06 +0300

    Вывод фотографии в отчет

--- admin-panel/src/app/system/assingment-info-page/assingment-ingo-page.component.ts
+++ admin-panel/src/app/system/assingment-info-page/assingment-ingo-page.component.ts
@@ -95,7 +95,7 @@ export class AssingmentIngoPageComponent implements OnInit {
     xhr.send();
   }
 
-  createReport() {
+  async createReport() {
     let doc = new jsPDF('p', 'pt', 'a4');
 
     doc.addFileToVFS("OpenSans-Regular.ttf", font)
@@ -106,10 +106,41 @@ export class AssingmentIngoPageComponent implements OnInit {
     let width = 300, height = 180;
 
     doc.text(this.assignment.task_title, x, y)
-    y = y + 40;
+    y = y + 40
 
-    this.steps.forEach(async (step: any) => {
+    // this.steps.forEach(async (step: any) => {
 
+    //   console.log(step.checklist)
+    //   doc.text(step.title, x, y)
+    //   y = y + 40;
+
+    //   doc.text("Checklist", x, y)
+    //   y = y + 30;
+
+    //   step.checklist.forEach((item: any) => {
+    //     doc.text(item.title, x, y)
+    //     x = x + 300
+    //     doc.text(item.isChecked.toString(), x, y)
+    //     x = x - 300
+    //     y = y + 20
+    //   })
+
+    //   if (step.report_file) {
+    //     const elemen = document.getElementById(step.id) as HTMLImageElement
+    //     const img = new Image();
+    //     img.src = elemen.src;
+
+    //     img.onload = () => {
+    //       doc.addImage(img, 'png', x, y, 250, 250);
+    //       console.log("Добавление")
+    //     };
+    //   }
+    // })
+
+
+
+    await this.steps.reduce(async (i: any, step: any) => {
+      await i;
       console.log(step.checklist)
       doc.text(step.title, x, y)
       y = y + 40;
@@ -125,71 +156,21 @@ export class AssingmentIngoPageComponent implements OnInit {
         y = y + 20
       })
 
+
+
       if (step.report_file) {
-
-
         const elemen = document.getElementById(step.id) as HTMLImageElement
-
-        let imageLoaded: HTMLImageElement = await this.getDataUri(elemen.src);
-        let pdf = new jsPDF('p', 'pt', 'a4');
-
-
         const img = new Image();
         img.src = elemen.src;
-        img.onload = () => {
-          // await for the image to be fully loaded
 
-          console.log(img)
-          doc.addImage(img, 'png', x, y, 250, 250);
-
-          doc.save("customFonts.pdf");
-          // doc.text("Sample", 20, 15);
-          // //...
-          // doc.save('order.pdf');
-        };
-
-        // pdf.addImage(elemen.src, "PNG", 0, 0, 250, 250);
-
-        // var img = document.createElement('img') as HTMLImageElement;
-        // img.src = step.report_file;
-
-        // console.log(img)
-        // doc.addImage(img, 'JPEG', 20, 20, 400, 150);
-
-        // const elemen = document.getElementById(step.id) as HTMLImageElement
-        // // // console.log(elemen, "ELEMEN")
-        // console.log(elemen.src)
-
-
-        // doc.addImage(elemen, 'PNG', 20, 20, 400, 150);
-        // y = y + 40;
-        // var myImage = new Image();
-        // myImage.src = step.report_file;
-
-        // myImage.onload = function () {
-        //   doc.addImage(myImage, 'png', x, y, width, height)
-        //   y = y + height + 20;
-        // };
-
-        // const srcpath = canvas.toDataURL(step.report_file);
-        // imgageData.src = srcpath;
-        // doc.addImage(imgageData, 'PNG', 20, 20, 400, 150);
+        const ans = await this.getDataUri(img.src)
+        await doc.addImage(img, 'png', x, y, 250, 250);
+        await console.log("Добавление")
       }
+    }, undefined);
 
 
-      // doc.addImage(step.report_string, 'PNG', x, y, width, height)
-      // y = y + height + 20;
-
-      // if (step.report_string) {
-
-      //   doc.text(step.title, x, y)
-      //   y = y + 40;
-
-      //   doc.addImage(step.report_string, 'PNG', x, y, width, height)
-      //   y = y + height + 20;
-      // }
-    })
-
+    console.log("Сохраняем")
     doc.save("customFonts.pdf");
   }
 
@@ -202,6 +183,15 @@ export class AssingmentIngoPageComponent implements OnInit {
       };
 
       image.src = url;
+    })
+  }
+
+  addImageProcess(src: string) {
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => resolve(img.height)
+      img.onerror = reject
+      img.src = src
     })
   }
 }
